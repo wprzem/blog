303921ec8fc5c8d5aadbf3d665ec0d2745f049bd
pw <wozniak.przem@gmail.com>
2021-04-20 21:41:37 +0200

    Added html templates and css

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'pw',
+        'title': 'blog post 1',
+        'content': 'first content',
+        'date_posted': '2021-04-15'
+    },
+    {
+        'author': 'ml',
+        'title': 'blog post 2',
+        'content': 'second content',
+        'date_posted': '2021-04-14'
+    }
+]
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
